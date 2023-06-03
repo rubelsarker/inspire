668e5b73e2910cdf68cb8e7951941e3cb44d1815
Rubel <spn14@spondonit.com>
2023-06-03 14:39:14 +0600

    add

--- src/Inspire.php
+++ src/Inspire.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Http;
 class Inspire
 {
     public function justDoIt() {
+
+        //ss
         $response = Http::get('https://inspiration.goprogram.ai/');
         return $response['quote'] . ' -' . $response['author'];
     }
